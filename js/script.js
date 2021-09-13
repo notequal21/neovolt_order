88e3677576d0b0c1358200ce1dc461a152069ea6
notequal21 <74978665+notequal21@users.noreply.github.com>
2021-09-13 09:56:48 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 let pickupBtn = document.querySelectorAll('.pick-up-method-radio')[0];
 let deliveryBtn = document.querySelectorAll('.pick-up-method-radio')[1];
 
+let individualBtn = document.querySelector('.individual');
+let entityBtn = document.querySelector('.entity');
+let individualBody = document.querySelectorAll('.yorcontact-body')[0];
+let entityBody = document.querySelectorAll('.yorcontact-body')[1];
+
 document.querySelector('.delivery').addEventListener('click', () => {
     if (pickupBtn.checked) {
         document.querySelector('.delivery-row').classList.add('disable');
@@ -11,4 +16,18 @@ document.querySelector('.delivery').addEventListener('click', () => {
         document.querySelectorAll('.delivery-col')[1].classList.add('disable');
         document.querySelector('.delivery__pickup').classList.add('disable');
     }
+});
+
+
+individualBtn.addEventListener('click', () => {
+    individualBtn.classList.add('active');
+    individualBody.classList.remove('hide');
+    entityBtn.classList.remove('active');
+    entityBody.classList.add('hide');
+});
+entityBtn.addEventListener('click', () => {
+    individualBtn.classList.remove('active');
+    individualBody.classList.add('hide');
+    entityBtn.classList.add('active');
+    entityBody.classList.remove('hide');
 });
